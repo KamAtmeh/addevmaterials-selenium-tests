0c41bf050ef76944643794d9ae81572cb884e419
[kamatmeh] <[kamal.atmeh@hotmail.com]>
2023-08-23 17:53:56 +0200

    Remove space

--- src/test/java/com/addev/hrportal/test/_03_A_SaisieComplexeTest.java
+++ src/test/java/com/addev/hrportal/test/_03_A_SaisieComplexeTest.java
@@ -13,7 +13,6 @@ import static org.junit.jupiter.api.Assertions.assertTrue;
 import static com.addev.hrportal.utils.DatabaseSQL.*;
 import static com.addev.hrportal.utils.Toolbox.*;
 
-
 @ExtendWith(ScreenshotOnFailureExtension.class)
 public class _03_A_SaisieComplexeTest extends _00_AbstractTest {
 
